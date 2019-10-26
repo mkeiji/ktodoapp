7afa15cde1a09ddb74b5d2444a330063f7759f73
keiji <keiji@email.com>
2019-10-26 00:15:19 -0500

    add klogo

--- app/src/components/layouts/Header.js
+++ app/src/components/layouts/Header.js
@@ -4,11 +4,17 @@ import { Link } from "react-router-dom";
 function Header() {
     return (
         <header style={headerStyle}>
+            <img src="/klogo.png" style={logoStyle} />
             <h1>TodoList</h1>
             <Link style={linkStyle} to="/">Home</Link> | <Link style={linkStyle} to="/about">About</Link>
         </header>
     );
 }
+
+const logoStyle = {
+    height: '80px',
+    width: '80px'
+};
 
 const linkStyle = {
     color: '#fff',
